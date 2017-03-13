639863cbb487ed9d775f46a5296c199d31a3e8e7
Lukas Kolletzki <lukas.kolletzki@tobit.software>
2017-03-13 15:05:18 +0100

    Fix build command

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,6 +3,8 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
@@ -16,13 +18,8 @@ module.exports = webpackMerge(commonConfig, {
 		chunkFilename: '[id].[hash].chunk.js'
 	},
 
-	htmlLoader: {
-		minimize: false
-	},
-
 	plugins: [
-		new webpack.NoErrorsPlugin(),
-		new webpack.optimize.DedupePlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.optimize.UglifyJsPlugin({
 			mangle: {
 				keep_fnames: true
@@ -32,6 +29,20 @@ module.exports = webpackMerge(commonConfig, {
 		new webpack.DefinePlugin({
 			'process.env': {
 				'ENV': JSON.stringify(ENV)
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				htmlLoader: {
+					minimize: false
+				}
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				postcss: function() {
+					return [precss, autoprefixer];
+				}
 			}
 		})
 	]
